cc80fffdf071ac77943729ef6ae66b063f8b8e0f
bwood <bwood@choicescreening.com>
2019-07-13 17:42:25 -0600

    good as I can do.

--- UnitTest.cs
+++ UnitTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace GeneralUnitTest
@@ -13,96 +14,114 @@ namespace GeneralUnitTest
     public class UnitTest
     {
 
-        public const int MAGIC_NUMBER = 15;
+        private static int MagicNumber;
+        private static int CenterNumber;
+
+        private static int[][] _testArray;
+        private static int[][] _solution;
 
         /// <summary>
         /// matrix, expected result
         /// </summary>
-        private static List<(int[][] matrix, int result)> _inputMatrices = new List<(int[][] matrix, int result)>
+        private static List<Tuple<int[][], int>> _inputMatrices = new List<Tuple<int[][], int>>
         {
-            (new[] { new[] { 4, 9, 2 },
-                     new[] { 3, 5, 7 },
-                     new[] { 8, 1, 5 } }, 1), // bottom right cell increased by 1
-            (new[] { new[] { 5, 3, 4 }, // 5 -> 8
-                     new[] { 1, 5, 8 }, // 8 -> 9
-                     new[] { 6, 4, 2 } }, 7), // 4 -> 7
-            (new[] { new[] { 4, 8, 2 }, // 8 -> 9
-                     new[] { 4, 5, 7 }, // 4 -> 3
-                     new[] { 6, 1, 6 } }, 4), // 6 -> 8
+            //new Tuple<int[][], int>( new int[][] { new int[] {4,9,2},
+            //                                       new int[] {3,5,7},
+            //                                       new int[] {8,1,5}}, 1), // bottom right cell increased by 1
+            //new Tuple<int[][], int>( new int[][] { new int[] {5,3,4}, // 5 -> 8 (cost 3)
+            //                                       new int[] {1,5,8}, // 8 -> 9 (cost 1)
+            //                                       new int[] {6,4,2}}, 7), // 4 -> 7 (cost 3)
+            //new Tuple<int[][], int>( new int[][] { new int[] {4,8,2}, // 8 -> 9
+            //                                       new int[] {4,5,7}, // 4 -> 3
+            //                                       new int[] {6,1,6}}, 4), // 6 -> 8
+            //new Tuple<int[][], int>( new int[][] { new int[] {4,5,8},
+            //                                       new int[] {2,4,1},
+            //                                       new int[] {1,9,7}}, 14),
+            //new Tuple<int[][], int>( new int[][] { new int[] {2,9,8},
+            //                                       new int[] {4,2,7},
+            //                                       new int[] {5,6,7}}, 21),
+            new Tuple<int[][], int>( new int[][] { new int[] {3,1,8},
+                                                   new int[] {2,5,8},
+                                                   new int[] {1,9,7}}, int.MaxValue), // an intermediate state from another matrix
         };
 
         [TestMethod]
         [DataRow(0)]
-        [DataRow(1)]
-        [DataRow(2)]
-        public void MagicSquare(int testMatrixIndex)
+        //[DataRow(1)]
+        //[DataRow(2)]
+        //[DataRow(3)]
+        public void UnitTest1(int testMatrixIndex)
         {
-            var result = formingMagicSquare(_inputMatrices[testMatrixIndex].matrix);
-            Assert.AreEqual(_inputMatrices[testMatrixIndex].result, result);
+            var result = formingMagicSquare(_inputMatrices[testMatrixIndex].Item1);
+            Assert.AreEqual(_inputMatrices[testMatrixIndex].Item2, result);
         }
 
-       /// <summary>
+        /// <summary>
         /// 
         /// </summary>
         /// <param name="s"></param>
         /// <returns></returns>
         static int formingMagicSquare(int[][] s)
         {
-            return GetMagicSquare(new Matrix(s), 0);
+            Initialize(s);
+            return Calc();
+        }
+
+        private static int Calc()
+        {
+            var sums = new List<int>(4);
+            foreach (var i in Enumerable.Range(1, 4))
+            {
+                PrintDebug();
+                int cost = CostDifference(_testArray);
+                sums.Add(cost);
+                Debug.Print($"Cost: {cost}");
+                _solution = RotateArray(_solution);
+            }
+            return sums.Min();
+        }
+
+        static private void Initialize(int[][] s)
+        {
+            _testArray = s;
+            IEnumerable<int> validNumbers = Enumerable.Range(1, 9);
+            MagicNumber = validNumbers.Sum() / s.Length;
+            CenterNumber = validNumbers.Count() / 2 + 1;
+            _solution = new int[][]{ new int[] {CenterNumber + 1,                                     MagicNumber - (CenterNumber + 1 + CenterNumber + 3),     CenterNumber + 3},
+                                     new int[] { MagicNumber - (CenterNumber + 1 + CenterNumber - 3), CenterNumber,                                            MagicNumber - (CenterNumber + 3 + CenterNumber - 1)},
+                                     new int[] {CenterNumber - 3,                                     MagicNumber - ((CenterNumber - 3) + (CenterNumber - 1)), CenterNumber - 1}};
+        }
+
+        static private void PrintDebug()
+        {
+            Debug.Print("--------");
+            Debug.Print($"{_testArray[0][0]}|{_testArray[0][1]}|{_testArray[0][2]} - {_solution[0][0]}|{_solution[0][1]}|{_solution[0][2]}");
+            Debug.Print($"{_testArray[1][0]}|{_testArray[1][1]}|{_testArray[1][2]} - {_solution[1][0]}|{_solution[1][1]}|{_solution[1][2]}");
+            Debug.Print($"{_testArray[2][0]}|{_testArray[2][1]}|{_testArray[2][2]} - {_solution[2][0]}|{_solution[2][1]}|{_solution[2][2]}");
         }
 
         /// <summary>
-        /// 
+        /// Return a new matrix rotated 90 to the left
         /// </summary>
-        /// <param name="currentMatrix"></param>
-        /// <param name="result">The sum of value changes made to individual cells required to complete the puzzle.</param>
         /// <returns></returns>
-        static int GetMagicSquare(Matrix currentMatrix, int result)
+        private static int[][] RotateArray(int[][] arr)
         {
-            ReplaceLine missingNumberReplacement = MissingNumberReplacement(currentMatrix);
-            ReplaceLine duplicateReplacement = currentMatrix.GetSubstituteForNextDuplicate();
-
-            (Matrix matrix, int result) nextCallFromMissingNumber = (currentMatrix.Clone(missingNumberReplacement.LineRef, missingNumberReplacement.ReplacementValue), result + missingNumberReplacement.AdjustValue);
-            (Matrix matrix, int result) nextCallFromDuplicate = (currentMatrix.Clone(duplicateReplacement.LineRef, duplicateReplacement.ReplacementValue), result + duplicateReplacement.AdjustValue);
-
-            /// Find the lesser replacement of the 2 techniques.  Tie goes to duplicate technique.
-            if ((missingNumberReplacement.LineRef != null) && (duplicateReplacement.LineRef != null))
-            {
-                if (missingNumberReplacement.AdjustValue < duplicateReplacement.AdjustValue)
-                {
-                    return GetMagicSquare(nextCallFromMissingNumber.matrix, nextCallFromMissingNumber.result);
-                } else if (duplicateReplacement.AdjustValue > missingNumberReplacement.AdjustValue)
-                {
-                    return GetMagicSquare(nextCallFromDuplicate.matrix, nextCallFromDuplicate.result);
-                }
-                else
-                {
-                    return GetMagicSquare(nextCallFromDuplicate.matrix, nextCallFromDuplicate.result);
-                }
-            } else if (missingNumberReplacement.LineRef != null)
-            {
-                return GetMagicSquare(nextCallFromMissingNumber.matrix, nextCallFromMissingNumber.result);
-            }
-            else if (duplicateReplacement.LineRef != null)
-            {
-                return GetMagicSquare(nextCallFromDuplicate.matrix, nextCallFromDuplicate.result);
-            }
-            else
-            {
-                // If neither method return anything, we're done.
-                return result;
-            }
+            return new int[][] { new int[] {arr[0][2], arr[1][2], arr[2][2]},
+                                 new int[] {arr[0][1], arr[1][1], arr[2][1]},
+                                 new int[] {arr[0][0], arr[1][0], arr[2][0]} };
         }
 
-        private static ReplaceLine MissingNumberReplacement(Matrix currentMatrix)
+        private static int CostDifference(int[][] other)
         {
-            ReplaceLine result = default;
-            int? nextMissingNumber = currentMatrix.NextMissingNumber();
-            if (nextMissingNumber.HasValue)
-            {
-                return currentMatrix.GetSubstituteForMissingFromHorzLines(nextMissingNumber.Value);
-            }
-            return result;
+            return Math.Abs(_solution[0][0] - other[0][0]) +
+                   Math.Abs(_solution[0][1] - other[0][1]) +
+                   Math.Abs(_solution[0][2] - other[0][2]) +
+                   Math.Abs(_solution[1][0] - other[1][0]) +
+                   Math.Abs(_solution[1][1] - other[1][1]) +
+                   Math.Abs(_solution[1][2] - other[1][2]) +
+                   Math.Abs(_solution[2][0] - other[2][0]) +
+                   Math.Abs(_solution[2][1] - other[2][1]) +
+                   Math.Abs(_solution[2][2] - other[2][2]);
         }
 
     }
